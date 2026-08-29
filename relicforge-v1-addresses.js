504579b89e4e66b5fd5152d4522bf2bc69dd7515
cryptoferd <cryptoferd1013@gmail.com>
2026-08-29 14:25:06 -0500

    Record canonical RC4.2 Sepolia V1 deployment

---
+++ relicforge-v1-addresses.js
@@ -0,0 +1,23 @@
+(function () {
+  'use strict';
+  window.RELICFORGE_V1_ADDRESSES = Object.freeze({
+    11155111: Object.freeze({
+      chainId: 11155111,
+      network: 'Ethereum Sepolia',
+      factory: '0x351769d3915Bbd2F5a9a4Fa34039D5B24730e3F5',
+      feePolicy: '0xfa1d96A9661EB3e6EC1221BDAf7e7F3043C3aF85',
+      collectionImplementation: '0xc4C0AE1C59072871395D7074276f777ba03667A1',
+      dataImplementation: '0xB110106B16A099DD9Cd4f3b002d3Dfa70e7fa70b',
+      renderer: '0x0dC94a4Bc89225FF1946CC089c42A437eFf9F2E3',
+      randomnessAdapter: '0x1A47d427dF215Fa039896b78e7fa1F35a93577a3',
+      ethUsdPriceFeed: '0x694AA1769357215DE4FAC081bf1f309aDC325306',
+      chainlinkVrfWrapper: '0x195f15F2d49d693cE265b4fB0fdDbE15b1850Cc1',
+      platformAdmin: '0x4667EEB3293ff97265A5e185EDE2e68Bc57dB055',
+      feeTreasury: '0xe098CF0AEcDbC59577b60eFC02D4C3F5A2d85AFE',
+      sponsoredFeeCents: 25,
+      minterFeeCents: 50,
+      maxCollectionFeeCents: 500,
+      sourceCommit: '9fe63f4667dd6defac8d79bd4bbeedf846eefc12'
+    })
+  });
+})();
